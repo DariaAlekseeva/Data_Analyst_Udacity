3f95a2fdf5699f7720d8f9dbf6db704237413886
Daria Alekseeva <alekseevadd@gmail.com>
2014-11-30 22:03:46 +0000

    Add MapReduce file, delete replic Wrangling file

--- MapReduce.py
+++ MapReduce.py
@@ -61,3 +61,339 @@ def word_count():
 
 word_count()
 
+
+
+
+
+import sys
+import string
+import logging
+
+from util import mapper_logfile
+logging.basicConfig(filename=mapper_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def mapper():
+
+    #Also make sure to fill out the reducer code before clicking "Test Run" or "Submit".
+
+    #Each line will be a comma-separated list of values. The
+    #header row WILL be included. Tokenize each row using the 
+    #commas, and emit (i.e. print) a key-value pair containing the 
+    #district (not state) and Aadhaar generated, separated by a tab. 
+    #Skip rows without the correct number of tokens and also skip 
+    #the header row.
+
+    #You can see a copy of the the input Aadhaar data
+    #in the link below:
+    #https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
+
+    #Since you are printing the output of your program, printing a debug 
+    #statement will interfere with the operation of the grader. Instead, 
+    #use the logging module, which we've configured to log to a file printed 
+    #when you click "Test Run". For example:
+    #logging.info("My debugging message")
+
+    #cycle through lines of data
+    for line in sys.stdin:
+        #tokenize the line of data
+        data = line.strip().split(",")
+        
+        if len(data) != 12 or data[0] == 'Registrar':
+            continue
+        print "{0}\t{1}".format(data[3],data[8])
+            
+mapper()
+
+
+
+
+
+import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def reducer():
+    
+    #Also make sure to fill out the mapper code before clicking "Test Run" or "Submit".
+
+    #Each line will be a key-value pair separated by a tab character.
+    #Print out each key once, along with the total number of Aadhaar 
+    #generated, separated by a tab. Make sure each key-value pair is 
+    #formatted correctly! Here's a sample final key-value pair: 'Gujarat\t5.0'
+
+    #Since you are printing the output of your program, printing a debug 
+    #statement will interfere with the operation of the grader. Instead, 
+    #use the logging module, which we've configured to log to a file printed 
+    #when you click "Test Run". For example:
+    #logging.info("My debugging message")
+    
+    aadhaar_generated = 0
+    old_key = None
+    
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        
+        if len(data) != 2:
+            continue
+        
+        this_key, count = data
+        
+        if old_key and old_key != this_key:
+            print "{0}\t{1}".format(old_key, aadhaar_generated)
+            aadhaar_generated = 0
+            
+        old_key = this_key
+        aadhaar_generated += float(count)
+        
+    if old_key != None:
+        print "{0}\t{1}".format(old_key, aadhaar_generated)
+            
+reducer()
+
+
+
+
+
+import sys
+import string
+import logging
+
+from util import mapper_logfile
+logging.basicConfig(filename=mapper_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def mapper():
+    """
+    The input to this mapper will be the final Subway-MTA dataset, the same as
+    in the previous exercise.  You can check out the csv and its structure below:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+
+    For each line of input, the mapper output should PRINT (not return) the UNIT as 
+    the key, the number of ENTRIESn_hourly as the value, and separate the key and 
+    the value by a tab. For example: 'R002\t105105.0'
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    
+    The logging module can be used to give you more control over your debugging
+    or other messages than you can get by printing them. In this exercise, print
+    statements from your mapper will go to your reducer, and print statements
+    from your reducer will be considered your final output. By contrast, messages
+    logged via the loggers we configured will be saved to two files, one
+    for the mapper and one for the reducer. If you click "Test Run", then we
+    will show the contents of those files once your program has finished running.
+    The logging module also has other capabilities; see 
+    https://docs.python.org/2/library/logging.html for more information.
+    """
+
+    for line in sys.stdin:
+        data = line.strip().split(",")
+        
+        if len(data) != 22 or data[1] == "UNIT":
+            continue
+        print "{0}\t{1}".format(data[1],data[6])
+
+
+mapper()
+
+
+
+
+import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def reducer():
+    '''
+    Given the output of the mapper for this exercise, the reducer should PRINT 
+    (not return) one line per UNIT along with the total number of ENTRIESn_hourly 
+    over the course of May (which is the duration of our data), separated by a tab.
+    An example output row from the reducer might look like this: 'R001\t500625.0'
+
+    You can assume that the input to the reducer is sorted such that all rows
+    corresponding to a particular UNIT are grouped together.
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    '''
+
+    entries = 0
+    old_key = None
+    
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        
+        if len(data) != 2:
+            continue
+            
+        this_key, count = data
+        
+        if old_key and old_key != this_key:
+            print "{0}\t{1}".format(old_key, entries)
+            entries = 0
+            
+        old_key = this_key
+        entries += float(count)
+        
+    if old_key != None:
+        print "{0}\t{1}".format(old_key, entries)
+        
+reducer()
+
+
+
+import sys
+import string
+import logging
+
+from util import mapper_logfile
+logging.basicConfig(filename=mapper_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def mapper():
+    '''
+    For this exercise, compute the average value of the ENTRIESn_hourly column 
+    for different weather types. Weather type will be defined based on the 
+    combination of the columns fog and rain (which are boolean values).
+    For example, one output of our reducer would be the average hourly entries 
+    across all hours when it was raining but not foggy.
+
+    Each line of input will be a row from our final Subway-MTA dataset in csv format.
+    You can check out the input csv file and its structure below:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+    
+    Note that this is a comma-separated file.
+
+    This mapper should PRINT (not return) the weather type as the key (use the 
+    given helper function to format the weather type correctly) and the number in 
+    the ENTRIESn_hourly column as the value. They should be separated by a tab.
+    For example: 'fog-norain\t12345'
+    
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    '''
+
+    # Takes in variables indicating whether it is foggy and/or rainy and
+    # returns a formatted key that you should output.  The variables passed in
+    # can be booleans, ints (0 for false and 1 for true) or floats (0.0 for
+    # false and 1.0 for true), but the strings '0.0' and '1.0' will not work,
+    # so make sure you convert these values to an appropriate type before
+    # calling the function.
+    def format_key(fog, rain):
+        return '{}fog-{}rain'.format(
+            '' if fog else 'no',
+            '' if rain else 'no'
+        )
+
+   
+    for line in sys.stdin:
+        data = line.strip().split(",")
+        
+        if len(data) != 22 or data[1] == "UNIT":
+            continue
+        print format_key(float(data[14]),float(data[15]))+"\t"+str(data[6])
+        
+        
+mapper()
+
+
+
+
+
+import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def reducer():
+    '''
+    Write a reducer that will compute the busiest date and time (that is, the 
+    date and time with the most entries) for each turnstile unit. Ties should 
+    be broken in favor of datetimes that are later on in the month of May. You 
+    may assume that the contents of the reducer will be sorted so that all entries 
+    corresponding to a given UNIT will be grouped together.
+    
+    The reducer should print its output with the UNIT name, the datetime (which 
+    is the DATEn followed by the TIMEn column, separated by a single space), and 
+    the number of entries at this datetime, separated by tabs.
+
+    For example, the output of the reducer should look like this:
+    R001    2011-05-11 17:00:00       31213.0
+    R002    2011-05-12 21:00:00       4295.0
+    R003    2011-05-05 12:00:00       995.0
+    R004    2011-05-12 12:00:00       2318.0
+    R005    2011-05-10 12:00:00       2705.0
+    R006    2011-05-25 12:00:00       2784.0
+    R007    2011-05-10 12:00:00       1763.0
+    R008    2011-05-12 12:00:00       1724.0
+    R009    2011-05-05 12:00:00       1230.0
+    R010    2011-05-09 18:00:00       30916.0
+    ...
+    ...
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    '''
+
+    max_entries = 0
+    old_key = None
+    datetime = ''
+    oldtime = ''
+
+    
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        
+        if len(data) != 4:
+            continue
+
+        this_key, date, time, entries = data
+        
+        if old_key and old_key != this_key:
+            print "{0}\t{1}\t{2}".format(old_key, datetime, float(max_entries))
+            max_entries = 0.0
+            datetime=''
+            oldtime = time
+        old_key = this_key
+        
+        if float(entries) > float(max_entries):
+            max_entries = float(entries)
+            datetime = str(date) + ' ' + str(time)
+        elif float(entries) == float(max_entries):
+            if time > oldtime:
+                datetime = str(date) + ' ' + str(time)
+            else:
+                datetime = str(date) + ' ' + str(oldtime)
+        
+    if old_key != None:
+        print "{0}\t{1}\t{2}".format(old_key, datetime, str(max_entries))
+reducer()
+
+
+
+
+
+
+
+
